e30f4c9a4a9f6d3f0af8df3b56885a0dd50d5c04
Oskar <oskar.branfeldt@gmail.com>
2022-12-06 19:33:20 +0100

    2022-12-02: solution

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,51 @@ pub fn day01() {
 fn test1() {
     day01();
 }
+
+fn day02_part1((a, x): (usize, usize)) -> usize {
+    if a == x {
+        x + 3
+    } else if x == (1 + a % 3) {
+        x + 6
+    } else {
+        x
+    }
+}
+
+fn day02_part2((a, x): (usize, usize)) -> usize {
+    let loose_value = if a - 1 == 0 { 3 } else { a - 1 };
+    let win_value = if a + 1 == 4 { 1 } else { a + 1 };
+    match x {
+        1 => loose_value,
+        2 => 3 + a,
+        3 => 6 + win_value,
+        _ => panic!(),
+    }
+}
+
+fn day02() {
+    let parts: Vec<&dyn Fn((usize, usize)) -> usize> = vec![&day02_part1, &day02_part2];
+    for (idx, part) in parts.iter().enumerate() {
+        let res: usize = include_str!("day2.input")
+            .split("\n")
+            .map(|gestures| -> usize {
+                gestures
+                    .split(" ")
+                    .filter_map(|play| match "ABCXYZ".find(play) {
+                        Some(play) => Some(1 + play % 3),
+                        _ => None,
+                    })
+                    .tuples()
+                    .map(part)
+                    .sum::<usize>()
+            })
+            .sum();
+
+        println!("Day 2 part {}: {}", idx + 1, res);
+    }
+}
+
+#[test]
+fn test2() {
+    day02();
+}
